99191c26c4c48374542502f41dd2d40ad8b900c9
peter88213 <petert88213@gmail.com>
2024-10-21 13:12:24 +0200

    Calculating and refreshing the statistics.

--- src/nvstatisticslib/statistics_viewer.py
+++ src/nvstatisticslib/statistics_viewer.py
@@ -6,6 +6,7 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from tkinter import ttk
 
+from novxlib.novx_globals import CH_ROOT
 from nvstatisticslib.nvstatistics_globals import _
 from nvstatisticslib.platform.platform_settings import PLATFORM
 from nvstatisticslib.platform.platform_settings import KEYS
@@ -29,8 +30,60 @@ class StatisticsViewer(tk.Toplevel):
         # "Close" button.
         ttk.Button(self, text=_('Close'), command=self.on_quit).pack(side='right', padx=5, pady=5)
 
+        self._mdl.register_client(self)
+        self.refresh()
+
     def on_quit(self, event=None):
         self._plugin.kwargs['window_geometry'] = self.winfo_geometry()
         self.destroy()
         self.isOpen = False
 
+    def refresh(self):
+        """Update the statistics."""
+        partWords = {}
+        chapterWords = {}
+        sectionWords = {}
+        stage1Words = {}
+        stage2Words = {}
+        partId = None
+        stage1Id = None
+        stage2Id = None
+        for chId in self._mdl.tree.get_children(CH_ROOT):
+            if self._mdl.novel.chapters[chId].chLevel == 1:
+                partId = chId
+                partWords[partId] = 0
+            else:
+                chapterWords[chId] = 0
+            if self._mdl.novel.chapters[chId].chType == 0:
+                for scId in self._mdl.tree.get_children(chId):
+                    if self._mdl.novel.sections[scId].scType == 0:
+                        sectionWords[scId] = self._mdl.novel.sections[scId].wordCount
+                        chapterWords[chId] += self._mdl.novel.sections[scId].wordCount
+                        if partId is not None:
+                            partWords[partId] += self._mdl.novel.sections[scId].wordCount
+                        if stage1Id is not None:
+                            stage1Words[stage1Id] += self._mdl.novel.sections[scId].wordCount
+                        if stage2Id is not None:
+                            stage2Words[stage2Id] += self._mdl.novel.sections[scId].wordCount
+                    elif self._mdl.novel.sections[scId].scType == 2:
+                        stage1Id = scId
+                        stage1Words[stage1Id] = 0
+                    elif self._mdl.novel.sections[scId].scType == 3:
+                        stage2Id = scId
+                        stage2Words[stage2Id] = 0
+
+        print('Parts')
+        for partId in partWords:
+            print(partId, partWords[partId])
+        print('Chapters')
+        for chId in chapterWords:
+            print(chId, chapterWords[chId])
+        print('Sections')
+        for scId in sectionWords:
+            print(scId, sectionWords[scId])
+        print('Stage 1st level')
+        for scId in stage1Words:
+            print(scId, stage1Words[scId])
+        print('Stage 2nd level')
+        for scId in stage2Words:
+            print(scId, stage2Words[scId])
