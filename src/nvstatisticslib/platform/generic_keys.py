9be4215285399aefaeeaf41c6e623758b74180ef
peter88213 <petert88213@gmail.com>
2024-12-02 10:39:19 +0100

    Fix docstring.

--- src/nvstatisticslib/platform/generic_keys.py
+++ src/nvstatisticslib/platform/generic_keys.py
@@ -1,7 +1,7 @@
 """Provide a class with key definitions.
 
 Copyright (c) 2024 Peter Triesberger
-For further information see https://github.com/peter88213/nv_progress
+For further information see https://github.com/peter88213/nv_statistics
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from nvstatisticslib.nvstatistics_globals import _
