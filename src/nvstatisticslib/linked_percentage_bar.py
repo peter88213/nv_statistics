828f4232041d1c1a54eb9655c8e09cb71dfcb7e0
peter88213 <petert88213@gmail.com>
2024-12-02 10:31:43 +0100

    Update docstring.

--- src/nvstatisticslib/linked_percentage_bar.py
+++ src/nvstatisticslib/linked_percentage_bar.py
@@ -1,7 +1,7 @@
 """Provide a class for a percentage bar that is linked to a tree view node.
 
 Copyright (c) 2024 Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/nv_statistics
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from mvclib.widgets.percentage_bar import PercentageBar
