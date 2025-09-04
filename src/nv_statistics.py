74caf3c62d8fb424fcc15b2f69a52dd42c7cd688
peter88213 <petert88213@gmail.com>
2025-09-04 12:58:09 +0200

    Updated docstring: Python 3.7+ required.

--- src/nv_statistics.py
+++ src/nv_statistics.py
@@ -1,6 +1,6 @@
 """Project statistics view plugin for novelibre.
 
-Requires Python 3.6+
+Requires Python 3.7+
 Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_statistics
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
